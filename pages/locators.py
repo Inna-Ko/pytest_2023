2dc067cf3842eb4220020d385744fe2dd78f04b8
Inna Konopleva <konopleva.in@yandex.ru>
2023-06-14 21:13:08 +0300

    Pages transition

---
+++ pages/locators.py
@@ -0,0 +1,8 @@
+class MainPageLocators():
+    LOGIN_LINK = ("css selector", "#registration_link")
+
+
+class LoginPageLocators():
+    LOGIN_URL = "login"
+    LOGIN_FORM = ("css selector", "#login_form")
+    REGISTER_FORM = ("css selector", "#register_form1")
