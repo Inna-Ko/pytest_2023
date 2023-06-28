88ff4cffe5cbf2237fb24336dc64fe5dc4d8ff26
Inna Konopleva <konopleva.in@yandex.ru>
2023-06-28 22:08:22 +0300

    welcome

--- test_welcome.py
+++
@@ -1,4 +0,0 @@
-from welcome import hello
-def test_hello():
-name = “Kiv”
-assert hello(name) == “Hello, Kiv”
---
+++ welcome/test_welcome.py
@@ -0,0 +1,6 @@
+from welcome import hello
+
+
+def test_hello():
+    name = "Kiv"
+    assert hello(name) == "Hello, Kiv", "Тестовое сообщение об ошибке"
